4143657fb44822a34d3c11165e2485fbfe90b8ed
RPaezA <136133391+RPaezA@users.noreply.github.com>
2024-12-02 10:32:21 -0500

    clase 14

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,6 +12,7 @@ import { AuthService } from '../../services/auth.service';
 export class LoginComponent {
 
   private authService=inject(AuthService)
+  private router=inject(Router)
   loginForm= new FormGroup({
     email:new FormControl("", [Validators.email, Validators.required]),
     password: new FormControl("", Validators.required)
@@ -20,6 +22,7 @@ export class LoginComponent {
     this.authService.loginConNest(this.loginForm.value).subscribe(
       (res)=>{
         console.log(res)
+        this.router.navigate(['/admin'])
       },
       (error)=>{
         console.log(error)
